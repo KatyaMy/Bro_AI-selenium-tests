98a67698cc0e832b78c5b8e9f316f55822d1e181
KatyaMy <20FamGam20@gmail.com>
2025-03-04 20:32:53 -0500

    Added test check_incorrect_password

--- data.py
+++ data.py
@@ -1,4 +1,9 @@
+#correct data
 user_name = 'tests_2@gmail.com'
 user_password = '12345678'
-
 alert_mms = 'Вы успешно залогинились'
+
+#incorrect data
+incorrect_name = 'tests_not_2@gmail.com'
+incorrect_password = '863DetyQA'
+expect_alert_text = 'Неверный логин или пароль'
--- test_sign_in_form.py
+++ test_sign_in_form.py
@@ -1,6 +1,7 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from data import user_name, user_password, alert_mms
+from data import user_name, user_password, alert_mms, expect_alert_text, incorrect_name, incorrect_password
 
 URL = 'http://95.182.122.183/login'
 
@@ -9,11 +10,11 @@ password = (By.CSS_SELECTOR, '#pass')
 submit_bt = (By.XPATH, '//button[@type="submit"]')
 alert_sms = (By.XPATH, '//div[@role="alert"]')
 
-#Check login
+#NOTE: Positive: Check login
 def test_check_correct_login(driver, wait):
     driver.get(URL)
-    driver.delete_all_cookies()
-    driver.refresh()
+    # driver.delete_all_cookies()
+    # driver.refresh()
     driver.find_element(*email).send_keys(user_name)
     driver.find_element(*password).send_keys(user_password)
     driver.find_element(*submit_bt).click()
@@ -21,5 +22,30 @@ def test_check_correct_login(driver, wait):
         EC.visibility_of_element_located(alert_sms))
     assert alert_txt.text == alert_mms, '_Not correct login or password'
 
+# NOTE: Negative: Check user using incorrect data
+@pytest.mark.xfail
+#FIXME
+def test_check_incorrect_login(driver, wait):
+    driver.get(URL)
+    # driver.delete_all_cookies()
+    # driver.refresh()
+    driver.find_element(*email).send_keys(incorrect_name)
+    driver.find_element(*password).send_keys(user_password)
+    driver.find_element(*submit_bt).click()
+    actual_alert_text = wait.until(
+        EC.visibility_of_element_located(alert_sms))
+    assert actual_alert_text.text == expect_alert_text, 'You are login'
 
-
+#NOTE: Negative: Check password using incorrect data
+#FIXME
+@pytest.mark.xfail
+def test_check_incorrect_password(driver, wait):
+    driver.get(URL)
+    # driver.delete_all_cookies()
+    # driver.refresh()
+    driver.find_element(*email).send_keys(user_name)
+    driver.find_element(*password).send_keys(incorrect_password)
+    driver.find_element(*submit_bt).click()
+    actual_alert_text = wait.until(
+        EC.visibility_of_element_located(alert_sms))
+    assert actual_alert_text.text == expect_alert_text, 'You are login'
